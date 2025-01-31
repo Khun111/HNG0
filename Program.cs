b10c4c4674e487b933fb3914492a055662a726cf
Oluwatobi <amure387@gmail.com>
2025-01-31 22:07:07 +0100

    fix: updated readme

--- Program.cs
+++ Program.cs
@@ -14,29 +14,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
 
 app.MapGet("/", () =>
 {
     return Results.Json(new { email = "amure387@gmail.com", current_datetime = DateTime.Now.ToString("yyyy-MM-ddTHH:mmZ"), github_url = "https://github.com/Khun111/HNG0" });
-    // var forecast =  Enumerable.Range(1, 5).Select(index =>
-    //     new WeatherForecast
-    //     (
-    //         DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-    //         Random.Shared.Next(-20, 55),
-    //         summaries[Random.Shared.Next(summaries.Length)]
-    //     ))
-    //     .ToArray();
-    // return forecast;
 })
 .WithName("Home");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
